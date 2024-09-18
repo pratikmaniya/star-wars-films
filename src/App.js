781e522b0bb25d113ec66a3eb04de6be65bc6053
Pratik Maniya <pratikmmaniya244@gmail.com>
2024-09-18 01:32:45 -0400

    deleted package-lock.json and added in .gitignore, fixed issues with tests getting failed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 
 import Header from './Components/Header';
 
@@ -8,18 +9,31 @@ import './Styles/App.css';
 const FilmList = React.lazy(() => import('./Pages/FilmList'));
 const FilmDetails = React.lazy(() => import('./Pages/FilmDetails'));
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#1976d2', // Custom primary color (blue)
+    },
+    secondary: {
+      main: '#dc004e', // Custom secondary color (red)
+    },
+  }
+});
+
 function App() {
   return (
-    <Router>
-      <Header />
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path="/" element={<FilmList />} />
-          <Route path="/films/:id" element={<FilmDetails />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Header />
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route path="/" element={<FilmList />} />
+            <Route path="/films/:id" element={<FilmDetails />} />
+          </Routes>
+        </Suspense>
+      </Router>
+    </ThemeProvider>
   );
 }
 
-export default App;
+export default App; 
--- src/index.js
+++ src/index.js
@@ -1,34 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { ApolloProvider } from '@apollo/client';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
 import App from './App';
 import client from './apollo/apollo-client';
-import ErrorBoundary from './ErrorBoundary'; 
+import ErrorBoundary from './ErrorBoundary';
 
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: '#1976d2', // Custom primary color (blue)
-    },
-    secondary: {
-      main: '#dc004e', // Custom secondary color (red)
-    },
-  }
-});
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={theme}>
-      <ApolloProvider client={client}>
-        <CssBaseline />
-        <ErrorBoundary>
-          <App />
-        </ErrorBoundary>
-      </ApolloProvider>
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <CssBaseline />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
+    </ApolloProvider>
   </React.StrictMode>
 );
